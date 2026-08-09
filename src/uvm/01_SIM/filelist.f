2528852d07b174d7f9af856d959516c8d9b805b7
문태성 (Taeseong Moon) <ansxotjd48@gmail.com>
2026-08-10 01:46:55 +0900

    Add files via upload

---
+++ src/uvm/01_SIM/filelist.f
@@ -0,0 +1,13 @@
+# RTL
+../00_RTL/apb_master.sv
+../00_RTL/baud_tick.v
+../00_RTL/bram.sv
+../00_RTL/fnd.sv
+../00_RTL/fnd_controller.v
+../00_RTL/gpio.sv
+../00_RTL/uart.sv
+../00_RTL/uart_tx.v
+../00_RTL/uart_rx.v
+
+# TOP
+tb/tb_apb_uvm.sv
